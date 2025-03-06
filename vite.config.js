c7ea71cfa852e086ff965fc37ef80bb414933ec3
TIPIAIM <alphaousmaneousmane@gmail.com>
2025-03-06 03:27:10 +0100

    Première3 action

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import sitemap from 'vite-plugin-sitemap';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
   plugins: [
     react(),
-    sitemap({ hostname: 'https://tiptamcode.vercel.app',
-      routes: ["/services", "/contact ", 
-        "apropos","realisation","/"]
-     }) // Remplace par ton domaine
-  ,
-],
+    sitemap({
+      hostname: "https://tiptamcode.com", //si tu ne lie pas sa ne marchera pas
+      routes: ["/services", "/contact ", "apropos", "realisation", "/"],
+    }), // Remplace par ton domaine
+  ],
 
-build: {
-  outDir: "dist", // Assurez-vous que la sortie est "dist"
-},
-
+  build: {
+    outDir: "dist", // Assurez-vous que la sortie est "dist"
+  },
 });
