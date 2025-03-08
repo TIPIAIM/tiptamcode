01ad3e4cd738f3c70ff07e68d54ad4fe9d51a607
TIPIAIM <alphaousmaneousmane@gmail.com>
2025-03-08 01:07:41 +0100

    Fix routes for Vercel6

--- src/Donnestructure.jsx
+++ src/Donnestructure.jsx
@@ -4,11 +4,10 @@ export default function StructuredData() {
       {JSON.stringify({
         "@context": "https://schema.org",
         "@type": "Website",
-        name: "AOD AVOCATS | Cabinet d'avocat Guinée-Conkry",
-        url: "https://aod-avocats.net",
+        name: "TIPTAMCode | Cabinet informatique",
+        url: "https://tiptamcode.com",
 
-        description:
-          " Notre cabinet est spécialisé en droit des affaires, droit civil, droit minier, droit des affaires ...",
+        description: " Notre cabinet est spécialisé en develloppement web ...",
         sameAs: [
           "https://web.facebook.com/people/AOD-Avocats-SCPA/61569855070493/?rdid=nu1hpcE1e9TKytvQ&share_url=https%3A%2F%2Fweb.facebook.com%2Fshare%2F15poedCHYP%2F%3F_rdc%3D1%26_rdr",
           "https://www.instagram.com/aod.avocats.net/",
@@ -19,8 +18,8 @@ export default function StructuredData() {
         },
         contactPoint: {
           "@type": "ContactPoint",
-          telephone: "+224 622 253 536",
-          contactType: "Service Juridique",
+          telephone: "+224 624 138 395",
+          contactType: "Service Informatique",
         },
       })}
     </script>
